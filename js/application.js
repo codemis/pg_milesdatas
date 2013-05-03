6a316e785e1e64bd93ad684cbf871879830d0d7a
Codemis <johnathan@missionaldigerati.com>
2013-05-02 21:35:01 -0700

    Added ability to select car.

--- js/application.js
+++ js/application.js
@@ -1,8 +1,17 @@
 var cars = ["Acura", "Honda"];
+var data = {'selectedCarIndex': ''};
 jQuery(document).ready(function($) {
 	$.each(cars, function(index, val) {
 	  //<a href="#" class="btn btn-large btn-primary" rel="car-one">Acura<br><i class="icon-auto"></i></a>
-		var carButton = $('<a/>').addClass("btn btn-large btn-primary btn-block").attr("rel", "car-"+index).html(val+"<br><i class='icon-auto'></i></a>");
+		var carButton = $('<a/>').addClass("btn btn-large btn-primary btn-block cars").attr("rel", index).html(val+"<br><i class='icon-auto'></i></a>");
 		$('#step-one').append(carButton);
+	});
+	$('a.cars').click(function() {
+		data['selectedCarIndex'] = $(this).attr("rel");
+		$('#step-two h3.title').text(cars[data['selectedCarIndex']]);
+		$('#step-one').fadeOut('slow', function() {
+			$('#step-two').fadeIn('slow');
+		});
+		return false;
 	});
 });
