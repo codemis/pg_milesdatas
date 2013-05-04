da7101b15489116c5f3d18107dd1060d7087f45a
Codemis <johnathan@missionaldigerati.com>
2013-05-04 06:02:34 -0700

    Added step nav, and submit button.

--- js/application.js
+++ js/application.js
@@ -1,39 +1,47 @@
 var cars = ["Acura", "Honda"];
 var data = {'selectedCarIndex': '', 'start': {}, 'stop': {}};
+var startingStepComplete = false;
+var stoppingStepComplete = false;
 jQuery(document).ready(function($) {
 	$.each(cars, function(index, val) {
 		var carButton = $('<a/>').addClass("btn btn-large btn-primary btn-block cars").attr("rel", index).html(val+"<br><i class='icon-auto'></i></a>");
 		$('#step-one').append(carButton);
 	});
-	if ( navigator.geolocation ) {
+	if(navigator.geolocation){
 		navigator.geolocation.getCurrentPosition(handleStartPosition);
 	}
 	$('a.cars').click(function() {
 		data['selectedCarIndex'] = $(this).attr("rel");
-		$('#step-two h3.title').text(cars[data['selectedCarIndex']]);
+		$('#step-nav h3.title').text(cars[data['selectedCarIndex']]);
 		$('#step-one').fadeOut('slow', function() {
-			$('#step-two').fadeIn('slow');
+			$('#step-nav').fadeIn('slow');
 		});
 		return false;
 	});
+	$('a.step-nav-link').click(function() {
+		var rel = $(this).attr('rel');
+		$('#step-nav').fadeOut('slow', function() {
+			$('#'+rel).fadeIn('slow');
+		});
+	});
 	$('form#starting-form').submit(function() {
 		setStartingFormData();
-		$('#step-three h3.title').text(cars[data['selectedCarIndex']]);
 		$('#step-two').fadeOut('slow', function() {
-			$('#step-three').fadeIn('slow');
+			$('a.step-nav-link[rel="step-two"]').children('i').removeClass('icon-exclamation-sign').addClass('icon-ok');
+			checkAndShowSaveButton();
+			$('#step-nav').fadeIn('slow');
 		});
 		return false;
 	});
 	$('form#stopping-form').submit(function(){
-		if ( navigator.geolocation ) {
+		if(navigator.geolocation){
 			navigator.geolocation.getCurrentPosition(handleStopPosition);
 		}
 		setStoppingFormData();
-		$('#step-confirm h3.title').text(cars[data['selectedCarIndex']]+"Trip Confirm");
 		$('#step-three').fadeOut('slow', function() {
-			$('#step-confirm').fadeIn('slow', function() {
-				alert(data['start']['lat']+" - "+data['start']['long']+"\r\n"+data['stop']['lat']+" - "+data['stop']['long']+"\r\n");
-			});
+			$('a.step-nav-link[rel="step-three"]').children('i').removeClass('icon-exclamation-sign').addClass('icon-ok');
+			checkAndShowSaveButton();
+			$('#step-nav').fadeIn('slow');
 		});
 		return false;
 	});
@@ -46,8 +54,15 @@ function handleStopPosition(pos) {
 };
 function setStartingFormData() {
 	$.extend(data['start'], {'location': $('input#starting-location').val(), 'odometer': $('input#starting-odometer').val(), 'reason': $('input#reason').val()});
+	startingStepComplete = true;
 };
 function setStoppingFormData() {
 	$.extend(data['stop'], {'location': $('input#stopping-location').val(), 'odometer': $('input#stopping-odometer').val()});
+	stoppingStepComplete = true;
+};
+function checkAndShowSaveButton() {
+	if((startingStepComplete === true) && (stoppingStepComplete === true)) {
+		$('a.step-nav-complete').removeClass('disabled');
+	}
 };
 
